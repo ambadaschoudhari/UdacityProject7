6eb8daaf09ad72147375974ebf51b3b1ff7c37e4
Ambadas Tulajadas Choudhari <ambadas_choudhari@ad.infosys.com>
2019-05-26 14:11:51 +0530

    Second commit of all code

---
+++ SourceCode/contracts/accesscontrol/CraftsmanRole.sol
@@ -0,0 +1,54 @@
+pragma solidity ^0.5.0;
+
+// Import the library 'Roles'
+import "./Roles.sol";
+
+// Define a contract 'CraftsManRole' to manage this role - add, remove, check
+contract CraftsManRole {
+  using Roles for Roles.Role;
+
+  // Define 2 events, one for Adding, and other for Removing
+  event CraftsManAdded(address indexed account);
+  event CraftsManRemoved(address indexed account);
+
+  // Define a struct 'craftsman' by inheriting from 'Roles' library, struct Role
+  Roles.Role private craftsmen;
+
+  // In the constructor make the address that deploys this contract the 1st craftsman
+  constructor() public {
+    _addCraftsMan(msg.sender);
+  }
+
+  // Define a modifier that checks to see if msg.sender has the appropriate role
+  modifier onlyCraftsMan() {
+    require(isCraftsMan(msg.sender));
+    _;
+  }
+
+  // Define a function 'isCraftsMan' to check this role
+  function isCraftsMan(address account) public view returns (bool) {
+    return craftsmen.has(account);
+  }
+
+  // Define a function 'addCraftsMan' that adds this role
+  function addCraftsMan(address account) public onlyCraftsMan {
+    _addCraftsMan(account);
+  }
+
+  // Define a function 'renounceCraftsMan' to renounce this role
+  function renounceCraftsMan() public {
+    _removeCraftsMan(msg.sender);
+  }
+
+  // Define an internal function '_addCraftsMan' to add this role, called by 'addCraftsMan'
+  function _addCraftsMan(address account) internal {
+    craftsmen.add(account);
+    emit CraftsManAdded(account);
+  }
+
+  // Define an internal function '_removeCraftsMan' to remove this role, called by 'removeCraftsMan'
+  function _removeCraftsMan(address account) internal {
+    craftsmen.remove(account);
+    emit CraftsManRemoved(account);
+  }
+}
---
+++ SourceCode/contracts/accesscontrol/WoodPickerRole.sol
@@ -0,0 +1,54 @@
+pragma solidity ^0.5.0;
+
+// Import the library 'Roles'
+import "./Roles.sol";
+
+// Define a contract 'WoodPickerRole' to manage this role - add, remove, check
+contract WoodPickerRole {
+  using Roles for Roles.Role;
+
+  // Define 2 events, one for Adding, and other for Removing
+  event WoodPickerAdded(address indexed account);
+  event WoodPickerRemoved(address indexed account);
+
+  // Define a struct 'woodpicker' by inheriting from 'Roles' library, struct Role
+  Roles.Role private woodpickers;
+
+  // In the constructor make the address that deploys this contract the 1st woodpicker
+  constructor() public {
+    _addWoodPicker(msg.sender);
+  }
+
+  // Define a modifier that checks to see if msg.sender has the appropriate role
+  modifier onlyWoodPicker() {
+    require(isWoodPicker(msg.sender));
+    _;
+  }
+
+  // Define a function 'isWoodPicker' to check this role
+  function isWoodPicker(address account) public view returns (bool) {
+    return woodpickers.has(account);
+  }
+
+  // Define a function 'addWoodPicker' that adds this role
+  function addWoodPicker(address account) public onlyWoodPicker {
+    _addWoodPicker(account);
+  }
+
+  // Define a function 'renounceWoodPicker' to renounce this role
+  function renounceWoodPicker() public {
+    _removeWoodPicker(msg.sender);
+  }
+
+  // Define an internal function '_addWoodPicker' to add this role, called by 'addWoodPicker'
+  function _addWoodPicker(address account) internal {
+    woodpickers.add(account);
+    emit WoodPickerAdded(account);
+  }
+
+  // Define an internal function '_removeWoodPicker' to remove this role, called by 'removeWoodPicker'
+  function _removeWoodPicker(address account) internal {
+    woodpickers.remove(account);
+    emit WoodPickerRemoved(account);
+  }
+}
